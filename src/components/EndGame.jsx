cb44ccd79f6806d95a906c8d2e4c55bf69ce0923
Manon <manonlefkos@gmail.com>
2023-08-31 15:33:31 +0200

    add EndGame component

---
+++ src/components/EndGame.jsx
@@ -0,0 +1,3 @@
+export default function EndGame({ score }) {
+  return <div>The game has ended with a score of {score}</div>;
+}
--- src/components/Game.jsx
+++ src/components/Game.jsx
@@ -1,28 +1,29 @@
 import { useState } from "react";
 import Cards from "./Cards";
+import EndGame from "./EndGame";
 
 export default function Game() {
   const [endGame, setEndGame] = useState(false);
   const [score, setScore] = useState(0);
+  const [highScore, setHighScore] = useState(0)
 
   function addPoint() {
     setScore(score + 1);
+    if (score + 1 > highScore) setHighScore(score + 1)
   }
 
   function gameComplete() {
     setEndGame(!endGame);
-    console.log("end game")
+    console.log("end game");
   }
 
-  if (!endGame) return (
-  <div>
-    <div>score: {score}</div>
-    <Cards scoreUp={addPoint} end={gameComplete} />
-  </div>
-  )
-  else return (
-    <div>
-      end score {score}
-    </div>
-  );
+  if (!endGame)
+    return (
+      <div>
+        <div>score: {score}</div>
+        <div>highScore: {highScore}</div>
+        <Cards scoreUp={addPoint} end={gameComplete} />
+      </div>
+    );
+  else return <EndGame score={score} />;
 }
