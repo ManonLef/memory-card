587f01b5c65c18bf50ccb45b685ee7facf610648
Manon <manonlefkos@gmail.com>
2023-08-31 15:33:17 +0200

    change useEffect position for API call

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -3,6 +3,38 @@ import { useEffect, useState } from "react";
 export default function Cards({ scoreUp, end }) {
   const [cardBacks, setCardBacks] = useState([]);
 
+  useEffect(() => {
+    console.log("fetching");
+    const fetchData = async () => {
+      const url = "https://omgvamp-hearthstone-v1.p.rapidapi.com/cardbacks";
+      const options = {
+        method: "GET",
+        headers: {
+          "X-RapidAPI-Key":
+            "2390dfc834mshaa88e9fce056958p13a829jsned1a12fc1efa",
+          "X-RapidAPI-Host": "omgvamp-hearthstone-v1.p.rapidapi.com",
+        },
+      };
+
+      try {
+        const response = await fetch(url, options);
+        const cardsData = await response.json();
+        console.log(cardsData)
+        // filter only cards with a back image
+        const cardsWithImg = cardsData.filter((card) =>
+          Object.prototype.hasOwnProperty.call(card, "img")
+        );
+        cardsWithImg.map(
+          (card) => ((card.key = card.cardBackId), (card.clicked = false))
+        );
+        setCardBacks(cardsWithImg);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+    fetchData();
+  }, []);
+
   function shuffle() {
     const shuffledArray = [...cardBacks];
     let currentIndex = shuffledArray.length,
@@ -37,46 +69,15 @@ export default function Cards({ scoreUp, end }) {
     }
   }
 
-  useEffect(() => {
-    console.log("fetching");
-    const fetchData = async () => {
-      const url = "https://omgvamp-hearthstone-v1.p.rapidapi.com/cardbacks";
-      const options = {
-        method: "GET",
-        headers: {
-          "X-RapidAPI-Key":
-            "c127c179b0mshd01e58517b9df72p14f875jsn27a0bd99e1cb",
-          "X-RapidAPI-Host": "omgvamp-hearthstone-v1.p.rapidapi.com",
-        },
-      };
-
-      try {
-        const response = await fetch(url, options);
-        const cardsData = await response.json();
-        // filter only cards with a back image
-        const cardsWithImg = cardsData.filter((card) =>
-          Object.prototype.hasOwnProperty.call(card, "img")
-        );
-        cardsWithImg.map(
-          (card) => ((card.key = card.cardBackId), (card.clicked = false))
-        );
-        setCardBacks(cardsWithImg);
-      } catch (error) {
-        console.error(error);
-      }
-    };
-    fetchData();
-  }, []);
-
   const cardsToShow = 10;
   return (
     <div>
       {cardBacks.slice(0, cardsToShow).map((back) => (
         <img
           key={back.key}
-          src={back.imgAnimated}
+          src={back.img}
           alt={back.name + " Heartstone card back illustration"}
-          width="150"
+          width="200"
           onClick={() => handleClick(back.key)}
         />
       ))}
