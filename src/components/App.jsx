1741e1a205a7edb10c07bd1959a64da63bdc24fb
Manon <manonlefkos@gmail.com>
2023-08-31 16:39:26 +0200

    add new Game button during testing for new mount

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,12 +1,20 @@
-import Game from "./Game"
+import { useState } from "react";
+import Game from "./Game";
 
 function App() {
+  const [highScore, setHighScore] = useState(0);
+  const [newGame, setNewGame] = useState(1)
+
+  function handleClick() {
+    setNewGame(newGame + 1)
+  }
 
   return (
     <div>
-      <Game />
+      <Game key={newGame} highScore={highScore} setHighScore={setHighScore} />
+      <button onClick={handleClick}>new game?</button>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
