52c0deec7866da40379c48c3a97bd61ed6ed7b9a
Manon <manonlefkos@gmail.com>
2023-09-02 22:14:55 +0200

    edit formatting

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,8 +1,8 @@
-export default function Card({key, src, onClick, alt}) {
+export default function Card({ key, src, onClick, alt }) {
   return (
     <div>
-      <img key={key} src={src} alt={alt} onClick={onClick} width="100"/>
+      <img key={key} src={src} alt={alt} onClick={onClick} width="100" />
       <div>{alt}</div>
     </div>
-  )
+  );
 }
