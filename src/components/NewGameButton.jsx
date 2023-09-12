d2a1a3c917c8a4946d7d5cd484dfbd0a070c33a9
Manon <manonlefkos@gmail.com>
2023-09-12 11:24:32 +0200

    add proptype validation to new game button

--- src/components/NewGameButton.jsx
+++ src/components/NewGameButton.jsx
@@ -1,9 +1,15 @@
+import PropTypes from "prop-types";
+
 export default function NewGameButton({ startNewGame }) {
   return (
     <button
       onClick={startNewGame}
-      className="uppercase tracking-widest m-4 bg-white text-fuchsia-500 hover:bg-fuchsia-200 text-white font-bold py-2 px-4 rounded">
+      className="uppercase tracking-widest m-4 bg-white text-fuchsia-500 hover:bg-fuchsia-200 font-bold py-2 px-4 rounded">
       play again?
     </button>
   );
 }
+
+NewGameButton.propTypes = {
+  startNewGame: PropTypes.func,
+};
