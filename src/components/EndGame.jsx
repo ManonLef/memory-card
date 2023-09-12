b620d12082d2209e985038258e17c04dc1307819
Manon <manonlefkos@gmail.com>
2023-09-12 11:22:01 +0200

    add EndGame prop validation

--- src/components/EndGame.jsx
+++ src/components/EndGame.jsx
@@ -1,5 +1,11 @@
-export default function EndGame({ score, highScore, setHighScore }) {
+import PropTypes from "prop-types";
+
+export default function EndGame({ score}) {
   return (
     <div className="text-xl font-bold p-4">game ended with {score} points </div>
   );
 }
+
+EndGame.propTypes = {
+  score: PropTypes.number,
+};
