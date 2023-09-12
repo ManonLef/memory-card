7892c03a653e3e69d1aee3b6feadde02cb2e3e09
Manon <manonlefkos@gmail.com>
2023-09-12 11:03:35 +0200

    update card style

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,7 +1,7 @@
 export default function Card({ id, src, onClick, alt }) {
   return (
     <div className="text-sm p-2 flex-col text-center bg-white bg-opacity-25 hover:bg-opacity-50 hover:bg-gray-100 rounded-lg">
-      <img className="p-1 mx-auto" key={id} src={src} alt={alt} onClick={onClick} width="170" />
+      <img className="p-1 mx-auto" key={id} src={src} alt={alt} onClick={onClick} width="150" loading="eager"/>
       <div>{alt}</div>
     </div>
   );
