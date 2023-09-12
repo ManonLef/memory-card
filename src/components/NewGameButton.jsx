162aedfd4ca64642f197ce787c699183cf3ec20a
Manon <manonlefkos@gmail.com>
2023-09-12 11:38:26 +0200

    edit button text color

--- src/components/NewGameButton.jsx
+++ src/components/NewGameButton.jsx
@@ -4,7 +4,7 @@ export default function NewGameButton({ startNewGame }) {
   return (
     <button
       onClick={startNewGame}
-      className="uppercase tracking-widest m-4 bg-white text-fuchsia-500 hover:bg-fuchsia-200 font-bold py-2 px-4 rounded">
+      className="uppercase tracking-widest m-4 bg-white text-indigo-500 hover:bg-fuchsia-200 font-bold py-2 px-4 rounded">
       play again?
     </button>
   );
